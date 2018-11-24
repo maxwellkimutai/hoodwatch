3be1f778526fafa113dcddf5d0b1ef279da957f8
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-24 08:15:26 +0300

    add business and post views

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,5 +1,85 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import UserProfile,Post,Neighborhood,Business,Comment
+from django.contrib.auth.decorators import login_required
+from .forms import UserProfileForm,BusinessForm,PostForm,CommentForm
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request,'index.html') 
+    current_user = request.user
+    try:
+        profile = UserProfile.objects.get(user = current_user)
+    except:
+        return redirect('edit_profile',username = current_user.username)
+
+    try:
+        posts = Post.objects.filter(neighborhood = profile.neighborhood)
+    except:
+        posts = None
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.neighborhood = profile.neighborhood
+            post.save()
+        return redirect('index')
+    else:
+        form = PostForm()
+    return render(request,'index.html',{"posts":posts,"profile":profile,"form":form})
+
+@login_required
+def edit_profile(request,username):
+    current_user = request.user
+    if request.method == 'POST':
+        profile = UserProfile.objects.get(user=current_user)
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('index')
+    elif UserProfile.objects.filter(user=current_user).exists():
+        profile = UserProfile.objects.get(user=current_user)
+        form = UserProfileForm(instance=profile)
+    else:
+        form = UserProfileForm()
+    return render(request,'edit_profile.html',{"form":form})
+
+@login_required
+def businesses(request):
+    current_user = request.user
+    neighborhood = UserProfile.objects.get(user = current_user).neighborhood
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = current_user
+            business.neighborhood = neighborhood
+            business.save()
+            return redirect('businesses')
+    else:
+        form = BusinessForm()
+
+    try:
+        businesses = Business.objects.filter(neighborhood = neighborhood)
+    except:
+        businesses = None
+
+    return render(request,'businesses.html',{"businesses":businesses,"form":form})
+@login_required
+def post(request,id):
+    post = Post.objects.get(id=id)
+    comments = Comment.objects.filter(post=post)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+        return redirect('post',id = post.id)
+    else:
+        form = CommentForm()
+    return render(request,'post.html',{"post":post,"comments":comments,"form":form})
