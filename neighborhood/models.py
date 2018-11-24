d5375e8f0e1e1c83c7cbc54539889da66c160fde
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-24 08:13:49 +0300

    add business and comments model

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -12,6 +12,9 @@ class Neighborhood(models.Model):
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'user_profile')
+    first_name = models.CharField(max_length = 50,null=True)
+    last_name = models.CharField(max_length = 50,null=True)
+    bio = models.TextField(null=True)
     neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
     email = models.EmailField(max_length = 60)
 
@@ -21,6 +24,7 @@ class UserProfile(models.Model):
 class Business(models.Model):
     name = models.CharField(max_length = 60)
     user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'business_user')
+    description = models.CharField(max_length = 150,null=True)
     neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE,related_name = 'business_neighbourhood')
     email = models.EmailField(max_length = 60)
 
@@ -32,3 +36,16 @@ class Post(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete = models.CASCADE,null=True)
+    comment = models.TextField()
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.comment
