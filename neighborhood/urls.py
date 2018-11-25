ecdfb26f2d9077e2ec7f4f9f67998ac1dae90661
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-25 16:51:56 +0300

    add search url

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url('^$',views.index,name = 'index'),
     url('^edit_profile/(?P<username>\w{0,50})',views.edit_profile,name = 'edit_profile'),
     url('^businesses$',views.businesses,name = 'businesses'),
-    url('^post/(?P<id>\d+)',views.post,name='post')
+    url('^post/(?P<id>\d+)',views.post,name='post'),
+    url(r'^search/$',views.search,name='search'),
+    url('^api/posts/$',views.PostList.as_view())
 
 ]
