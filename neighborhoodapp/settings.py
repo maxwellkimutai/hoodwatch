17f86e95a30298999ae44632edb4a9c2b3802d0c
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-25 20:26:09 +0300

    deploying to heroku

--- neighborhoodapp/settings.py
+++ neighborhoodapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'neighborhood',
-    'bootstrap4'
+    'bootstrap4',
+    'materializeform',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,10 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = '/'
+
+# Email configurations remember to install python-decouple
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
