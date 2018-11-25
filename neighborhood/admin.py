bd1429a41b6fc8043e7417c320484b7051c6862d
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-25 16:50:38 +0300

    register new models on admin site

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Neighborhood,UserProfile,Business,Post,Comment
+from .models import Neighborhood,UserProfile,Business,Post,Comment,Location,Category
 # Register your models here.
 admin.site.register(Neighborhood)
 admin.site.register(UserProfile)
 admin.site.register(Business)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Location)
+admin.site.register(Category)
