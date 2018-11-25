ed89a1ab79f1b27a4af96b38d222cfe662cfe3df
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-11-25 20:21:43 +0300

    add model methods

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -6,9 +6,29 @@ class Neighborhood(models.Model):
     name = models.CharField(max_length = 50)
     location = models.ForeignKey('Location',on_delete = models.CASCADE,null = True)
     admin = models.ForeignKey(User,on_delete = models.CASCADE)
+    occupants = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls,neigborhood_id):
+        neighborhood = cls.objects.get(id = neigborhood_id)
+        return neighborhood
+
+    def update_neighborhood(self):
+        self.save()
+
+    def update_occupants(self):
+        self.occupants += 1
+        self.save()
+
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'user_profile')
@@ -32,11 +52,26 @@ class Business(models.Model):
     def __str__(self):
         return self.name
 
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls,business_id):
+        business = Business.objects.get(id = business_id)
+        return business
+
+    def update_business(self):
+        self.save()
+
 class Post(models.Model):
     title = models.CharField(max_length = 50)
     content = models.TextField()
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    type = models.CharField(max_length = 50,null=True)
     pub_date = models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
